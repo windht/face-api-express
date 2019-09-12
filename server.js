63e9b1dc57c971786a55ce549967b39dffee83e8
Tony Hu Tong <buildmindht@outlook.com>
2019-09-12 16:53:09 +0800

    Added fd only endpoint and relay

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const util = require("./util");
+const axios = require("axios");
 
 const app = express();
 const port = process.env.PORT || 9000;
@@ -13,6 +14,19 @@ app.use(bodyParser.json({ limit: "50mb" }));
 app.post("/detect", async (req, res) => {
   try {
     const detections = await util.detect(Buffer.from(req.body.image, "base64"));
+    res.send(detections);
+  } catch (err) {
+    console.log(err);
+    res.status(400).send(err);
+  }
+});
+
+app.post("/detect-matches-descriptor", async (req, res) => {
+  try {
+    const detections = await util.detectMatchesWithDescriptors({
+      _image: Buffer.from(req.body.image, "base64"),
+      _faces: req.body.faces
+    });
     res.send(detections);
   } catch (err) {
     console.log(err);
@@ -44,6 +58,53 @@ app.post("/detect-matches", async (req, res) => {
       draw: req.body.draw
     });
     res.send({ detections, detectedImage });
+  } catch (err) {
+    console.log(err);
+    res.status(400).send(err);
+  }
+});
+
+app.post("/detect-matches-relay", async (req, res) => {
+  try {
+    const { detections } = await util.detectMatches({
+      _image:
+        req.body.image.includes("http://") ||
+        req.body.image.includes("https://")
+          ? req.body.image
+          : Buffer.from(
+              req.body.image.replace(/^data:image\/jpeg;base64,/, ""),
+              "base64"
+            ),
+      _faces: req.body.faces.map(face => ({
+        name: face.name,
+        image:
+          face.image.includes("http://") || face.image.includes("https://")
+            ? face.image
+            : Buffer.from(
+                face.image.replace(/^data:image\/jpeg;base64,/, ""),
+                "base64"
+              )
+      }))
+    });
+
+    let result = {};
+    if (req.body.url) {
+      try {
+        const { data } = await axios.post(
+          req.body.url,
+          {
+            detections
+          },
+          {
+            timeout: 2000
+          }
+        );
+        result = data;
+      } catch (err) {
+        result = detections;
+      }
+    }
+    res.send(result);
   } catch (err) {
     console.log(err);
     res.status(400).send(err);
--- util.js
+++ util.js
@@ -2,57 +2,104 @@ require("@tensorflow/tfjs-node");
 
 const faceapi = require("face-api.js");
 const minConfidence = 0.5;
-const faceDetectionOptions = new faceapi.SsdMobilenetv1Options({ minConfidence });
+const faceDetectionOptions = new faceapi.SsdMobilenetv1Options({
+  minConfidence
+});
 const canvas = require("canvas");
 const { Canvas, Image, ImageData } = canvas;
-faceapi.env.monkeyPatch({ Canvas, Image, ImageData })
+faceapi.env.monkeyPatch({ Canvas, Image, ImageData });
 
 module.exports = {
-    init: async()=>{
-        await faceapi.nets.ssdMobilenetv1.loadFromDisk('./models');
-        await faceapi.nets.faceLandmark68Net.loadFromDisk('./models')
-        await faceapi.nets.faceRecognitionNet.loadFromDisk('./models')
-    },
-    detect: async (_image) => {
-        const image = await canvas.loadImage(_image)
-        const detections = await faceapi.detectAllFaces(image, faceDetectionOptions);
-        return detections;
-    },
-    detectMatches: async({_image,_faces,draw}) => {
-        const image = await canvas.loadImage(_image);
-        const labeledDescriptors = await Promise.all(_faces.map(async face=>{
-            const faceImage = await canvas.loadImage(face.image);
-            const faceImageFd = await faceapi.detectSingleFace(faceImage).withFaceLandmarks().withFaceDescriptor()
-            return new faceapi.LabeledFaceDescriptors(face.name,[faceImageFd.descriptor])
-        }))
-        const faceMatcher = new faceapi.FaceMatcher(labeledDescriptors);
-        const results = await faceapi
-        .detectAllFaces(image)
-        .withFaceLandmarks()
-        .withFaceDescriptors();
-        const detections = results.map(fd=>({
-            ...fd,
-            match:faceMatcher.findBestMatch(fd.descriptor)
-        }));
+  init: async () => {
+    await faceapi.nets.ssdMobilenetv1.loadFromDisk("./models");
+    await faceapi.nets.faceLandmark68Net.loadFromDisk("./models");
+    await faceapi.nets.faceRecognitionNet.loadFromDisk("./models");
+  },
+  detect: async _image => {
+    const image = await canvas.loadImage(_image);
+    const detections = await faceapi.detectAllFaces(
+      image,
+      faceDetectionOptions
+    );
+    return detections;
+  },
+  detectMatches: async ({ _image, _faces, draw }) => {
+    const image = await canvas.loadImage(_image);
+    const labeledDescriptors = await Promise.all(
+      _faces.map(async face => {
+        const faceImage = await canvas.loadImage(face.image);
+        const faceImageFd = await faceapi
+          .detectSingleFace(faceImage)
+          .withFaceLandmarks()
+          .withFaceDescriptor();
+        return new faceapi.LabeledFaceDescriptors(face.name, [
+          faceImageFd.descriptor
+        ]);
+      })
+    );
+    const faceMatcher = new faceapi.FaceMatcher(labeledDescriptors);
+    const results = await faceapi
+      .detectAllFaces(image)
+      .withFaceLandmarks()
+      .withFaceDescriptors();
+    const detections = results.map(fd => ({
+      ...fd,
+      match: faceMatcher.findBestMatch(fd.descriptor)
+    }));
 
-        let detectedImage;
+    let detectedImage;
 
-        if (draw) {
-            const _canvas = canvas.createCanvas(image.width, image.height);
-            const ctx = _canvas.getContext('2d');
-            ctx.drawImage(image, 0, 0)
-            detections.forEach(detection=>{
-                if (detection.match.distance < 0.4) {
-                    const drawBox = new faceapi.draw.DrawBox(detection.detection.box, {
-                        label:detection.match.label + " " + detection.match.distance,
-                        boxColor: "rgb(0,100,0)"
-                    })
-                    drawBox.draw(_canvas);
-                }
-            })
-            detectedImage = _canvas.toDataURL('image/jpeg');
+    if (draw) {
+      const _canvas = canvas.createCanvas(image.width, image.height);
+      const ctx = _canvas.getContext("2d");
+      ctx.drawImage(image, 0, 0);
+      detections.forEach(detection => {
+        if (detection.match.distance < 0.4) {
+          const drawBox = new faceapi.draw.DrawBox(detection.detection.box, {
+            label: detection.match.label + " " + detection.match.distance,
+            boxColor: "rgb(0,100,0)"
+          });
+          drawBox.draw(_canvas);
         }
-
-        return {detections,detectedImage};
+      });
+      detectedImage = _canvas.toDataURL("image/jpeg");
     }
-}
+
+    return { detections, detectedImage };
+  },
+  detectMatchesWithDescriptors: async ({ _image, _faces, draw }) => {
+    const image = await canvas.loadImage(_image);
+    const labeledDescriptors = _faces.map(
+      face => new faceapi.LabeledFaceDescriptors(face.name, face.descriptors)
+    );
+    const faceMatcher = new faceapi.FaceMatcher(labeledDescriptors);
+    const results = await faceapi
+      .detectAllFaces(image)
+      .withFaceLandmarks()
+      .withFaceDescriptors();
+    const detections = results.map(fd => ({
+      ...fd,
+      match: faceMatcher.findBestMatch(fd.descriptor)
+    }));
+
+    let detectedImage;
+
+    if (draw) {
+      const _canvas = canvas.createCanvas(image.width, image.height);
+      const ctx = _canvas.getContext("2d");
+      ctx.drawImage(image, 0, 0);
+      detections.forEach(detection => {
+        if (detection.match.distance < 0.4) {
+          const drawBox = new faceapi.draw.DrawBox(detection.detection.box, {
+            label: detection.match.label + " " + detection.match.distance,
+            boxColor: "rgb(0,100,0)"
+          });
+          drawBox.draw(_canvas);
+        }
+      });
+      detectedImage = _canvas.toDataURL("image/jpeg");
+    }
+
+    return { detections, detectedImage };
+  }
+};
